50c2a7251efed5bfcd900dfb0edeb917c4e23fd8
Mauricio Landeros <jairo.landeros2885@alumnos.udg.mx>
2022-03-06 15:00:27 -0600

    Message broadcast

--- index.js
+++ index.js
@@ -12,7 +12,12 @@ app.use(express.static('public'));
 var io = socket(server);
 io.on('connection', function(socket){
     console.log('Made socket connection', socket.id);
+
     socket.on('chat', function(data){
         io.sockets.emit('chat', data);
-    })
+    });
+
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
 })
